ee11ea27d5f842a7eb1bc0778c6b91d4c7b6d5dc
OXAYR <uzairmaqsood451@gmail.com>
2023-09-19 00:36:41 +0500

    reservation model & controller formed

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const cartController = require('../app/api/controllers/cart');
-router.get('/:userId', cartController.getCart);
-router.post('/', cartController.addToCart);
-router.put('/updatetickets/:id', cartController.updateTickets);
-router.delete('/:movieId', cartController.removeFromCart)
-
+const cartController = require("../app/api/controllers/reservations");
+router.get("/:userId", cartController.getReservations);
+router.post("/", cartController.makeAReservation);
+router.put("/updatetickets/:id", cartController.updateReservation);
+router.delete("/:movieId", cartController.removeFromReservation);
 
 module.exports = router;
