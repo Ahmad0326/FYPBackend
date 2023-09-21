2501d5811147dc132e2ba7497e3061f93cb22b63
OXAYR <uzairmaqsood451@gmail.com>
2023-09-22 00:09:21 +0500

    delete manager end point is formed

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,8 @@ router.post("/register", userController.create);
 //router.post('/register', validateRegistration, userController.create);
 router.post("/authenticate", userController.authenticate);
 router.get("/all", userController.getAllUsers);
-router.put("/:managerId", userController.deleteManager);
 router.put("/:userId/updateUser", userController.updateUser);
+router.delete("/:managerId", userController.deleteManager);
 router.delete("/:userId", userController.deleteUser);
 
 module.exports = router;
